b6e44009aadd33afef2d292a67573656986a51ae
JVrhar <126876959+JVrhar@users.noreply.github.com>
2023-03-03 12:59:10 +0100

    Add files via upload

---
+++ Euro Kalkulator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+double kn;
+double suma;
+cout<<"Upisi cijenu u eurima:";
+cin>>kn;
+suma= kn*7.53;
+cout<< "Cijena u kunama je:"<<suma;
+
+}
+
